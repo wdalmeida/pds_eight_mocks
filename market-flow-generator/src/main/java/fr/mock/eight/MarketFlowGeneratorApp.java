ded4e10656f133775f2aa1cf4c1868576aa5528e
Sellathurai Vyach <sellathurai.vyach@yahoo.fr>
2017-11-26 00:20:41 +0100

    init generator projec

---
+++ market-flow-generator/src/main/java/fr/mock/eight/MarketFlowGeneratorApp.java
@@ -0,0 +1,13 @@
+package fr.mock.eight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketFlowGeneratorApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketFlowGeneratorApp.class, args);
+    }
+
+}
