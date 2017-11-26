d85b50097381f81c27622e842f11641133d09483
tom.lerouge <tom.g.lerouge@gmail.com>
2017-11-26 20:03:26 +0100

    minor bug fixed: bankservice module was not declared within the parent project

--- bankservice/src/main/java/controller/RestApiController.java
+++ bankservice/src/main/java/controller/RestApiController.java
@@ -17,7 +17,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/externalBank")
 public class RestApiController {
 
     public static final Logger logger = LoggerFactory.getLogger(RestApiController.class);
