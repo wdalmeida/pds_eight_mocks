d35e58a5e9100b4094ef09ef5d68d44ab640e9fc
Sellathurai Vyach <sellathurai.vyach@yahoo.fr>
2017-11-26 17:42:03 +0100

    manage time in scheduler

--- market-flow-generator/src/main/java/fr/mock/eight/schedule/ScheduledTasks.java
+++ market-flow-generator/src/main/java/fr/mock/eight/schedule/ScheduledTasks.java
@@ -6,6 +6,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -17,13 +18,43 @@ public class ScheduledTasks {
 
     private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
 
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    private static final SimpleDateFormat dateHourFormat = new SimpleDateFormat("HH:mm:ss");
 
+
+    private Date initialDate = new Date();
+    private Date iterateDate = new Date();
+    private Date currentDate;
+
+    private int i = 0;
+    private int moins = -1;
 
     @Scheduled(fixedDelay = 60000)
     public void reportCurrentTime() {
 
-        log.info("The time is now {}", dateFormat.format(new Date()));
+        currentDate = new Date();
+
+        log.info("i : " +i);
+
+
+
+        log.info("Initial Date : "+initialDate);
+        log.info("current date : "+currentDate);
+
+
+        //set date plus 1 minutes ago
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(iterateDate);
+        calendar.add(Calendar.MINUTE, moins);
+
+        iterateDate = calendar.getTime();
+
+        log.info("moins 1min date : "+iterateDate);
+
+        i++;
+
+
+
+
     }
 
 
