ba775fb7ad3b8c0e38425b12ea52ba5c424fc67e
vyach.sellathurai <vyach.sellathurai@etu.u-pec.fr>
2017-12-06 00:08:18 +0100

    resolve issues

--- market-flow-generator/src/main/java/fr/mock/eight/MarketFlowGeneratorApp.java
+++ market-flow-generator/src/main/java/fr/mock/eight/MarketFlowGeneratorApp.java
@@ -1,11 +1,15 @@
 package fr.mock.eight;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class MarketFlowGeneratorApp {
 
     public static void main(String[] args) {
