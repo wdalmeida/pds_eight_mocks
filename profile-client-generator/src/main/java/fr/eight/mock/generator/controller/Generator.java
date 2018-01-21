4dfc5721d684b57afbd0d82dbf5856d3f1e9bd24
vyach.sellathurai <vyach.sellathurai@etu.u-pec.fr>
2018-01-21 23:21:29 +0100

    intagration of rules

--- profile-client-generator/src/main/java/fr/eight/mock/generator/controller/Generator.java
+++ profile-client-generator/src/main/java/fr/eight/mock/generator/controller/Generator.java
@@ -1,6 +1,7 @@
 package fr.eight.mock.generator.controller;
 
 import com.opencsv.CSVWriter;
+import fr.eight.mock.generator.service.ValuesGenerator;
 import fr.eight.mock.generator.utils.Options;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,11 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Random;
 
-/**
- * Created by Vyach on 18/01/2018.
- */
 @RestController
 public class Generator {
 
@@ -23,113 +20,13 @@ public class Generator {
     @Autowired
     private Options options;
 
-    /**
-     * Incrementale
-     */
-    private int idCustomer = 1;
-
-    /**
-     * Incrementale
-     */
-    private int idAccount = 1;
-
-    /**
-     * a definir mais semble incrementale
-     */
-    private int idHistoricPurchase = 1;
-
-    /**
-     * est genere : OK
-     */
-    private int age;
-
-    /**
-     *  est genere OK :
-     *
-     *  1 : sans emploi
-     *  2 : etudiant
-     *  3 : employe
-     *  4 : ouvriers
-     *  5 : cadres
-     *  6 : retraite
-     */
-    private int catSocial;
-
-    /**
-     * est genere : OK
-     *
-     *  1 : celibataire
-     *  2 : marie
-     *  3 : veuf
-     *  4 : pacse
-     */
-    private int statusFamilial;
-
-    /**
-     * est genere, 0 < x < 4 : OK
-     */
-    private int nbrEnfants;
-
-    /**
-     * est genere : OK
-     *
-     *  1 : courant
-     *  2 : livret jeune
-     *  3 : PEA
-     *  4 : PEL
-     */
-    private int typeCompte;
-
-    /**
-     *  est genere : OK
-     *
-     *  1 : est proprietaire
-     *  2 : nest pas proprietaire
-     */
-    private int estProprietaire;
-
-
-
-    /**
-     * est genere : OK
-     */
-    private int solde;
-
-    /**
-     *  1 : sans emploi
-     *  2 : etudiant
-     *  3 : employe
-     *  4 : ouvriers
-     *  5 : cadres
-     *  6 : retraite
-     * @return
-     */
-
-
-    /**
-     * est genere : OK
-     */
-    private int revenu;
-
-    /**
-     * est genere
-     */
-    private int creditHisto;
-
-
-
-    /**
-     * est genere
-     */
-    private int creditCourant;
-
+    private int idCustomer = 1, idAccount = 1, idHistoricPurchase = 1;
 
 
 
     @RequestMapping("/launch")
     public String generateProfils() throws IOException {
 
-        String response = "The generator is running...";
         String row = "";
 
         CSVWriter csvWriter = new CSVWriter(new FileWriter(this.options.getFileName()));
@@ -137,28 +34,20 @@ public class Generator {
                 "familial_status", "childs", "account_type", "is_owner", "balance", "income", "loan_histo", "Y"});
         for (int i = 0; i < options.getProfilsNombre(); i++) {
 
-            //generation of age
-            this.age = this.ageGeneration();
-            //generation of familial status
-            this.statusFamilial = this.setFamilialStatus();
-            //generation of childs
-            this.nbrEnfants = this.setChilds();
-            //generation of type account
-            this.typeCompte = this.getTypeAccount();
-
-            this.estProprietaire = this.setOwner();
-
-            this.catSocial = this.genSocialCat();
-
-            this.solde = this.setSolde();
-
-            this.creditHisto = this.generateHistoCredit();
-
-            this.creditCourant = this.generateCurrentCredit();
+            int age = ValuesGenerator.generateAge();
+            int statusFamilial = ValuesGenerator.generateFamilialStatus();
+            int nbrEnfants = ValuesGenerator.generateChilds(age);
+            int typeCompte = ValuesGenerator.generateTypeAccount(age);
+            int estProprietaire = ValuesGenerator.genereateIsOwner();
+            int catSocial = ValuesGenerator.generateSocialCategory(age);
+            int solde = ValuesGenerator.generateBalance(catSocial);
+            int creditHisto = ValuesGenerator.generateLoanHistory(age);
+            int creditCourant = ValuesGenerator.generateCurrentLoan(creditHisto);
+            int revenu = ValuesGenerator.generateRevenue(catSocial);
 
 
             if (age > 63) { catSocial = 6; }
-            revenu = this.calculateRevenu();
+
 
 
             //writing in csv process
@@ -166,7 +55,7 @@ public class Generator {
                     idCustomer+"",
                     idAccount+"",
                     idHistoricPurchase+"",
-                    age+"",
+                    age +"",
                     catSocial+"",
                     statusFamilial+"",
                     nbrEnfants+"",
@@ -178,259 +67,16 @@ public class Generator {
                     creditCourant+""
             });
 
-            //incrémentation
+            //incrementation
             idCustomer++;idAccount++;idHistoricPurchase++;
 
         }
 
         csvWriter.close();
 
-        response="Generation done !";
+        String response = "Generation done !";
 
         return response;
     }
 
-
-    private int ageGeneration() {
-
-        Random rn = new Random();
-
-        int nextAge = rn.nextInt(88 - 18 + 1) + 18;
-
-        return nextAge;
-    }
-
-    /**
-     *  est genere :
-     *
-     *  1 : sans emploi  11%
-     *  2 : etudiant  22%
-     *  3 : employe  24%
-     *  4 : ouvriers 17%
-     *  5 : cadres 9%
-     *  6 : retraite 16%
-     */
-    private int genSocialCat() {
-
-        Random rn = new Random();
-
-        int ratio = rn.nextInt(100 - 0 + 1) + 0;
-
-        log.info("ratio : " + ratio);
-
-        int catSocial = 6;
-
-        if (ratio < 100) {
-            catSocial = 6;
-        }
-        if (ratio < 83) {
-            catSocial = 5;
-        }
-        if (ratio < 74) {
-            catSocial = 4;
-        }
-        if (ratio < 57) {
-            catSocial = 3;
-        }
-        if (ratio < 33) {
-            catSocial = 2;
-        }
-        if (ratio < 11) {
-            catSocial = 1;
-        }
-
-
-        return catSocial;
-
-    }
-
-    private int getTypeAccount() {
-
-        int nextAccountType = this.typeCompte;
-
-        Random rn = new Random();
-
-
-        if (age < 26) {
-            int ratio = rn.nextInt(10 - 0 + 1) + 10;
-
-            if (ratio > 4) {
-                nextAccountType = 2;
-            }
-
-        }
-        else {
-            int ratio = rn.nextInt(101);
-
-            if (ratio < 101) {
-                nextAccountType = 1;
-            }
-            if (ratio < 56) {
-                nextAccountType = 3;
-            }
-            if (ratio < 39) {
-                nextAccountType = 4;
-            }
-        }
-
-        return nextAccountType;
-    }
-
-    /**
-     *  1 : sans emploi
-     *  2 : etudiant
-     *  3 : employe
-     *  4 : ouvriers
-     *  5 : cadres
-     *  6 : retraite
-     * @return
-     */
-    private int calculateRevenu() {
-
-        Random rn = new Random();
-
-        int revenue = 0;
-
-        if (catSocial == 2) {
-            revenue = rn.nextInt(1200 - 600 + 1) + 600;
-            revenue = revenue - 100;
-        }
-        else if (catSocial == 3) {
-            revenue = rn.nextInt(2000 - 1200 + 1) + 1200;
-            revenue = revenue + 300;
-        }
-        else if (catSocial == 4) {
-            revenue = rn.nextInt(1900 - 1000 + 1) + 1000;
-            revenue = revenue - 150;
-        }
-        else if (catSocial == 5) {
-            revenue = rn.nextInt(2600 - 1800 + 1) + 1800;
-            revenue = revenue + 600;
-        }
-        else if (catSocial == 6) {
-            revenue = rn.nextInt(1200 - 700 + 1) + 700;
-            revenue = revenue - 200;
-        }
-
-        return revenue;
-
-    }
-
-    private int setFamilialStatus() {
-
-        int familialStatus = 1;
-
-        Random rn = new Random();
-
-        int ratio = rn.nextInt(100 - 0 + 1) + 0;
-
-        if(ratio < 100) {
-            familialStatus = 4;
-        }
-        if(ratio < 86) {
-            familialStatus = 3;
-        }
-        if(ratio < 79) {
-            familialStatus = 2;
-        }
-        if (ratio < 33) {
-            familialStatus = 1;
-        }
-
-        return familialStatus;
-    }
-
-
-    private int setChilds() {
-
-        Random rn = new Random();
-
-        int childs = rn.nextInt(4 - 0 + 1) + 1;
-
-        return childs;
-    }
-
-    private int setOwner() {
-
-        int isOwner = 1;
-        Random rn = new Random();
-
-        int ratio = rn.nextInt(101);
-
-        if (ratio > 39) {
-            isOwner = 0;
-        }
-
-        return isOwner;
-    }
-
-    private int setSolde() {
-
-        Random rn = new Random();
-        int solde = 0;
-
-        if (catSocial == 1 || catSocial == 2 || catSocial == 6) {
-
-            int ratio = rn.nextInt(10 - 0 + 1) + 0;
-
-            if (ratio < 3) {
-                solde = rn.nextInt(2001);
-                solde = -solde;
-            }
-            else {
-                solde = rn.nextInt(3000 - 0 + 1) + 0;
-            }
-        }
-        else if(catSocial == 3 ||catSocial == 4) {
-            int ratio = rn.nextInt(10 - 0 + 1) + 0;
-
-            if (ratio < 2) {
-                solde = rn.nextInt(2001);
-                solde = -solde;
-            }
-            else {
-                solde = rn.nextInt(5000 - 0 + 1) + 0;
-            }
-        }
-        else if(catSocial == 5) {
-            solde = rn.nextInt(7000 - 1500 + 1) + 1500;
-        }
-
-        return solde;
-    }
-
-    private int  generateHistoCredit() {
-        Random rn = new Random();
-
-        int histoCredit = 0;
-        int ratio = histoCredit = rn.nextInt(101);
-
-        if (ratio > 95) {
-            histoCredit = 0;
-        }
-        else {
-            histoCredit = rn.nextInt(1001);
-        }
-
-        return  histoCredit;
-    }
-
-    private int generateCurrentCredit() {
-
-        int credit = 0;
-
-        Random rn = new Random();
-
-        int ratio = rn.nextInt(101);
-
-        if (ratio > 88) {
-            credit = 0;
-        }
-        else {
-            credit = (this.creditHisto / 2) + 100;
-        }
-
-        return credit;
-
-    }
 }
---
+++ profile-client-generator/src/main/java/fr/eight/mock/generator/service/ValuesGenerator.java
@@ -0,0 +1,279 @@
+package fr.eight.mock.generator.service;
+
+import java.util.Random;
+
+
+public class ValuesGenerator {
+
+
+    /**
+     *
+     * @param min !< 0
+     * @param max !< 0
+     * @return random number between min & max inclusive
+     */
+    public static int randInt(int min, int max) {
+
+        Random rn = new Random();
+
+        return rn.nextInt((max - min) + 1) + min;
+
+    }
+
+    /**
+     *
+     * @return an age between 18 & 88 years old
+     */
+    public static int generateAge() {
+        return randInt(18, 88);
+    }
+
+    /**
+     *  1 : celibataire
+     *  2 : marie
+     *  3 : veuf
+     *  4 : pacse
+     * @return familial status
+     */
+    public static int generateFamilialStatus() {
+
+        int familialStatus = 1;
+
+        int ratio = randInt(0, 100);
+
+        if(ratio < 100) {
+            familialStatus = 4;
+        }
+        if(ratio < 86) {
+            familialStatus = 3;
+        }
+        if(ratio < 79) {
+            familialStatus = 2;
+        }
+        if (ratio < 33) {
+            familialStatus = 1;
+        }
+
+        return familialStatus;
+    }
+
+    /**
+     * RULE 1 : < 20 : 0 childrens else between 0 and 5
+     * @return number of childs between 0 and 4
+     */
+    public static int generateChilds(int age) {
+
+        if (age >= 20) {
+            return randInt(0, 5);
+        }
+        else {
+            return 0;
+        }
+
+    }
+
+    /**
+     *  Courant    1
+     *  Livret Jeune    2
+     *  PEA    3
+     *  PEL    4
+     *  RULE 1 : if age < 26: more probability to get an account type 2
+     * @param age must be >= 18
+     * @return type account
+     */
+    public static int generateTypeAccount(int age) {
+
+        int typeAccount = 1;
+
+        if (age <= 26) {
+            int ratio = randInt(0, 10);
+
+            if (ratio > 4) { typeAccount = 2; }
+
+        }
+        else {
+            int ratio = randInt(0, 100);
+
+            if (ratio < 100) { typeAccount = 1; }
+            if (ratio < 56)  { typeAccount = 3; }
+            if (ratio < 39)  { typeAccount = 4; }
+        }
+
+        return typeAccount;
+    }
+
+
+    /**
+     * RULE 2 : more probability to not be owner than be owner
+     * @return is owner 1 or not 2
+     */
+    public static int genereateIsOwner() {
+
+        int isOwner = 1;
+
+        int ratio = randInt(0, 100);
+
+        if (ratio > 39) {
+            isOwner = 0;
+        }
+
+        return isOwner;
+    }
+
+    /**
+     *  1 : sans emploi  11%
+     *  2 : etudiant  22%
+     *  3 : employe  24%
+     *  4 : ouvriers 17%
+     *  5 : cadres 9%
+     *  6 : retraite 16%
+     *  @param age betwteen 18 and 88
+     * @return a social category
+     */
+    public static int generateSocialCategory(int age) {
+        int ratio = randInt(0, 100);
+
+        int catSocial = 6;
+
+
+
+        if (age <= 26) {
+
+            if (ratio < 100) { catSocial = 2; }
+            if (ratio < 33)  { catSocial = 3; }
+            if (ratio < 7) { catSocial = 1; }
+
+        }
+        else if (age > 63) {
+            catSocial = 6;
+        }
+        else {
+            if (ratio < 100) {
+                catSocial = 5; // cadre : 8%
+            }
+            if (ratio < 92) {
+                catSocial = 4; // ouvriers : 33%
+            }
+            if (ratio < 59) {
+                catSocial = 3; // employe : 47 %
+            }
+            if (ratio < 12) {
+                catSocial = 1; // sans emploi : 12
+            }
+
+        }
+
+        return catSocial;
+    }
+
+    /**
+     *
+     * @param socialCat [1,6]
+     * @return a balance in function of the social category
+     */
+    public static int generateBalance(int socialCat) {
+        int solde = 0;
+
+        if (socialCat == 2) {
+            solde = randInt(1000, 3000);
+        }
+        else if (socialCat == 1 || socialCat == 6) {
+            int ratio = randInt(0, 10);
+
+            if (ratio < 3) {
+                solde = randInt(0, 2000);
+                solde = -solde;
+            }
+            else {
+                solde = randInt(0, 3000);
+            }
+        }
+        else if(socialCat == 3 ||socialCat == 4) {
+            int ratio = randInt(0, 10);
+
+            if (ratio < 2) {
+                solde = randInt(0, 2000);
+                solde = -solde;
+            }
+            else {
+                solde = randInt(0, 3500);
+            }
+        }
+        else if(socialCat == 5) {
+            solde = randInt(1500, 7000);
+        }
+
+        return solde;
+    }
+
+    public static int generateLoanHistory(int age) {
+
+        int histoCredit;
+
+
+        if (age < 26) {
+            histoCredit = 0;
+        } else {
+            int ratio = randInt(0, 100);
+
+            if (ratio > 97) {
+                histoCredit = 0;
+            }
+            else {
+                histoCredit = randInt(0, 500);
+            }
+
+        }
+
+        return  histoCredit;
+    }
+
+    /**
+     *
+     * @param loanHistory !< 0
+     * @return the current loan
+     */
+    public static int generateCurrentLoan(int loanHistory) {
+        int credit;
+
+        int ratio = randInt(0, 100);
+
+        if (ratio > 88) {
+            credit = 0;
+        }
+        else {
+            credit = (loanHistory / 2) + 100;
+        }
+
+        return credit;
+    }
+
+    public static int generateRevenue(int socialCat) {
+
+        int revenue = 0;
+
+        if (socialCat == 2) {
+            revenue = randInt(0, 2600);
+            revenue = revenue - 100;
+        }
+        else if (socialCat == 3) {
+            revenue = randInt(1200, 2000);
+            revenue = revenue + 300;
+        }
+        else if (socialCat == 4) {
+            revenue = randInt(1000, 1900);
+            revenue = revenue - 150;
+        }
+        else if (socialCat == 5) {
+            revenue = randInt(1800, 14400);
+            revenue = revenue + 600;
+        }
+        else if (socialCat == 6) {
+            revenue = randInt(700, 1200);
+            revenue = revenue - 200;
+        }
+
+        return revenue;
+    }
+
+}
