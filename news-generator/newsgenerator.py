87d5c6a03aa809d27d1632a98cf3baedf814986b
Warren <warren@local>
2018-02-15 10:53:07 +0100

    Timestamp float to integer

--- news-generator/newsgenerator.py
+++ news-generator/newsgenerator.py
@@ -12,7 +12,7 @@ def main():
     for y in range(10):
      text.append(fake.text(1000))
     date= datetime.now().strftime('%Y-%m-%d%H%M%S%f')
-    ts = datetime.datetime.now().timestamp()
+    ts = int(datetime.datetime.now().timestamp())
     link= "http://rss.eight.inside.esiag.info/news/news"+date+".html"
     print(date)
     f= open("/var/www/html/news/news%s.html" % (date),"w+")
