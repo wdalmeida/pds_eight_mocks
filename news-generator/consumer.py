0dc9f845d16f3b6e87f71ebb9ccf1bc4e7ebe824
Warren <warren@local>
2018-02-16 10:45:50 +0100

    Resolve consumer issue

--- news-generator/consumer.py
+++ news-generator/consumer.py
@@ -3,13 +3,18 @@ import json
 
 from utils import writeXML
 
+print("Start consumer")
 consumer = KafkaConsumer('news',
                              group_id='rssConsumer',
-                             bootstrap_servers=['10.10.1.110:9092','10.10.1.111:9092'], auto_offset_reset='latest',
+                             bootstrap_servers=['10.10.1.110:9092','10.10.1.111:9092'],
+                             auto_offset_reset='latest',
                              enable_auto_commit=True)
+print("Consumer")
 for message in consumer:
+    print("message")
     print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
                                          message.offset, message.key,
                                          message.value.decode('utf-8')))
     json_data = message.value.decode('utf-8')
     writeXML.create(json.loads(json_data))
+    print("end of for")
